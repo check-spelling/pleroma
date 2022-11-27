8f9bc89eb440abe4b0d75df40b43b1873eeebe0a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-26 23:29:19 -0500

    spelling: notifications
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- lib/pleroma/bbs/handler.ex
+++ lib/pleroma/bbs/handler.ex
@@ -92,7 +92,7 @@ defmodule Pleroma.BBS.Handler do
     IO.puts("r <id> <text> - Reply to the post with the given id")
     IO.puts("t <id> - Show a thread from the given id")
     IO.puts("n - Show notifications")
-    IO.puts("n read - Mark all notifactions as read")
+    IO.puts("n read - Mark all notifications as read")
     IO.puts("f <id> - Favourites the post with the given id")
     IO.puts("R <id> - Repeat the post with the given id")
     IO.puts("quit - Quit")
