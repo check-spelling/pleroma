4b800af73d86bc50845493a8e4d350b8a1ca2d0a
Josh Soref <2119212+jsoref@users.noreply.github.com>
2022-11-26 23:30:23 -0500

    spelling: quarantined
    Signed-off-by: Josh Soref <2119212+jsoref@users.noreply.github.com>

--- priv/repo/migrations/20201005124600_quarantained_policy_string_to_tuple.exs
+++ priv/repo/migrations/20201005124600_quarantained_policy_string_to_tuple.exs
@@ -2,7 +2,7 @@
 # Copyright © 2017-2022 Pleroma Authors <https://pleroma.social/>
 # SPDX-License-Identifier: AGPL-3.0-only
 
-defmodule Pleroma.Repo.Migrations.QuarantainedStringToTuple do
+defmodule Pleroma.Repo.Migrations.QuarantinedStringToTuple do
   use Ecto.Migration
 
   alias Pleroma.ConfigDB
